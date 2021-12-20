5f8ed1f09bd976f1f9b1f4a50b8520478ceb4d3f
Joshua P. Hendrix <joshuaphendrix@gmail.com>
2021-12-19 22:24:35 -0500

    this might break

--- src/app.js
+++ src/app.js
@@ -30,6 +30,33 @@ var server = http.createServer(function (req, res) {
         try{
             let args = url.parse(req.url,true).query;
             log('Recived'+args); 
+
+            var AWS = require("aws-sdk");
+
+            AWS.config.update({
+              region: "us-east-1",
+              endpoint: "http://localhost:8000"
+            });
+            
+            var docClient = new AWS.DynamoDB.DocumentClient();
+            
+            var params = {
+                TableName:"Log",
+                Item:{
+                    "LogID": 1,
+                    "InsertDate": new Date()*1,
+                    "info":args
+                }
+            };
+            
+            console.log("Adding a new item...");
+            docClient.put(params, function(err, data) {
+                if (err) {
+                    console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("Added item:", JSON.stringify(data, null, 2));
+                }
+            });            
         }catch(err){
             log(err);
         }
