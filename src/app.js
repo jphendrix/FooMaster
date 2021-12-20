2928f6e1bd11d865bfa13ac03d6344cd757665ca
Joshua P. Hendrix <joshuaphendrix@gmail.com>
2021-12-20 17:03:53 -0500

    we good?

--- src/app.js
+++ src/app.js
@@ -13,49 +13,35 @@ var log = function(entry) {
 const server = http.createServer();
 
 server.on('request', async (req,res)=>{
-    let response = {action:"none"};
-
-    log("Starting Up");
     try{
-        
-        log("22");
         let args = url.parse(req.url,true).query;
     
-        log("24");
         log("URL Args:" + JSON.stringify(args));
-        log("26");
-        log("put is a " + typeof put);
 
-        //if(args.data){
-        //    if(typeof args.data === "string"){
-        //        args.data = JSON.parse(args.data);
-        //    }
+        if(args.data){
+            if(typeof args.data === "string"){
+                args.data = JSON.parse(args.data);
+            }
     
-            put()
-                .then((x)=>{response = x})
-                .catch((x)=>{response = x});
-        //}
-    
-        log("after await" + JSON.stringify(response));
+            put(args.data)
+                .then((x)=>{
+                    res.end(JSON.stringify(x));
+                })
+                .catch((x)=>{
+                    res.end(JSON.stringify(x));
+                });
+        }
 
     }catch(err){
-        log("inside catch");
-        log("err is " + typeof err);
-        log(err+'');
-        log(err);
-        log(JSON.stringify(err));
+        res.end(JSON.stringify(err));
     }
 
-    res.end(JSON.stringify(response));
+    res.end(JSON.stringify({action:"none"}));
 });
 
-function put(){
-    log("38")
+function put(d){
     return new Promise(resolve => {
-        log("40")
         try{
-            log("Trying the best I can");
-
             var AWS = require("aws-sdk");
             AWS.config.update({
                 region:"us-east-1",
@@ -68,27 +54,23 @@ function put(){
                 Item:{
                     "InsertDate":new Date()*1,
                     "Source":"Primary",
-                    "Data":"balls"
-                },
-                TableName:"Journal"
+                    "Data":JSON.stringify(d)
+                },TableName:"Journal"
             };
     
             log("Adding: " + JSON.stringify(item));
-            try{
-                docClient.put(item,function(err,data){
-                    if(err){
-                        log("Err putting: " + JSON.stringify(err));
-                        resolve({err:JSON.stringify(err)});
-                    }else{
-                        log("Great Scott!" + JSON.stringify(data));
-                        resolve({success:JSON.stringify(data)});
-                    }
-                });
-            }catch(e){
-                resolve({e:JSON.stringify(e)});
-            }
-        }catch(e1){
-            resolve({e1:JSON.stringify(e1)});
+
+            docClient.put(item,function(err,data){
+                if(err){
+                    log("Err putting: " + JSON.stringify(err));
+                    resolve({error:JSON.stringify(err)});
+                }else{
+                    log("Great Scott!" + JSON.stringify(data));
+                    resolve({success:JSON.stringify(data)});
+                }
+            });
+        }catch(err){
+            resolve({error:JSON.stringify(err)});
         }
     });
 }
